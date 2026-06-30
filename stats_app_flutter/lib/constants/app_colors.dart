ade8baf6991872aab2fd993cf66284121cb8c629
ManddalorianV <akifsayrim@gmail.com>
2026-06-30 23:19:38 +0300

    app colors updated.

--- stats_app_flutter/lib/constants/app_colors.dart
+++ stats_app_flutter/lib/constants/app_colors.dart
@@ -4,8 +4,8 @@ class AppColors {
   const AppColors._();
 
   static const Color background = Color(0xFF000000);
-  static const Color cardBackground = Color(0xFF101012);
-  static const Color trackBackground = Color(0xFF1C1C1F);
+  static const Color cardBackground = Color(0xFF000000);
+  static const Color trackBackground = Color(0xFF000000);
 
   static const Color activeGreen = Color(0xFF4ADE80);
   static const Color inactiveRed = Color(0xFFEF4444);
